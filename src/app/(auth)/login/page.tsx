3e22395ff0c9a1fed13b2147f8be5dc3f470c61d
Your Name <you@example.com>
2026-01-29 14:20:10 +0700

    fix logic of login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import FieldPassword from "@/components/shared/FieldPassword";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -5,57 +7,114 @@ import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Info } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/store/useAuthStore";
 
-const page = () => {
+const Page = () => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const setUser = useAuthStore((state) => state.setUser); 
+
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const employeeCode = formData.get("employeeCode");
+    const password = formData.get("password");
+
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            employeeCode,
+            password,
+          }),
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("Login failed");
+      }
+
+      const data = await res.json();
+
+      setUser({
+      username: data.user.employeeCode,
+      role: data.user.role,
+      permissions: data.user.permissions ?? [],
+      });
+
+
+router.push("/dashboard");
+    } catch (err) {
+      setError("Invalid username or password");
+    }
+  };
+
   return (
     <form
-      action=''
-      method='post'
-      className=' border border-primary bg-card rounded-2xl px-5 py-6 shadow-2xl'
+      onSubmit={handleLogin}
+      className="border border-primary bg-card rounded-2xl px-5 py-6 shadow-2xl"
     >
-      <p className='font-semibold text-3xl mb-6 text-center'>Welcome Back</p>
+      <p className="font-semibold text-3xl mb-6 text-center">
+        Welcome Back
+      </p>
 
       <FieldGroup>
-        <Field className='gap-1'>
-          <FieldLabel className='text-md' htmlFor='username'>
-            Username
+        <Field className="gap-1">
+          <FieldLabel className="text-md" htmlFor="employeeCode">
+            Employee Code
           </FieldLabel>
           <Input
-            className='border-primary outline-none ring-0'
-            type='text'
-            id='username'
-            name="username"
-            placeholder='Enter Username'
+            className="border-primary outline-none ring-0"
+            type="text"
+            id="employeeCode"
+            name="employeeCode"
+            placeholder="Ex: M000001"
             required
           />
         </Field>
 
-        <FieldPassword name='password' label='Password' />
+        <FieldPassword name="password" label="Password" />
       </FieldGroup>
 
-      <div className='text-sm flex mt-2 mb-4 justify-between '>
+      <div className="text-sm flex mt-2 mb-4 justify-between">
         <FieldGroup>
-          <Field orientation='horizontal'>
-            <Checkbox name='remember' id='remember' />
-            <FieldLabel htmlFor='remember'>Remember me</FieldLabel>
+          <Field orientation="horizontal">
+            <Checkbox name="remember" id="remember" />
+            <FieldLabel htmlFor="remember">
+              Remember me
+            </FieldLabel>
           </Field>
         </FieldGroup>
+
         <Link
-          className='flex-1 text-nowrap decoration-1 underline hover:text-primary-hover'
-          href={"/forgot"}
+          className="flex-1 text-nowrap underline hover:text-primary-hover"
+          href="/forgot"
         >
           Forgot Your Password?
         </Link>
       </div>
 
-      {/* { <div className="flex items-center gap-2 p-2 mb-2 bg-danger/10 rounded-lg text-danger">
-        <Info className="size-4" />
-        <span>Invalid Password and Username</span>
-      </div>} */}
+      {error && (
+        <div className="flex items-center gap-2 p-2 mb-2 bg-danger/10 rounded-lg text-danger">
+          <Info className="size-4" />
+          <span>{error}</span>
+        </div>
+      )}
 
       <Button
-        size='lg'
-        className='w-full cursor-pointer hover:bg-primary-hover'
+        type="submit"
+        size="lg"
+        className="w-full cursor-pointer hover:bg-primary-hover"
       >
         Login
       </Button>
@@ -63,4 +122,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
