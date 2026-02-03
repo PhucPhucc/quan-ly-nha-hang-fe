cb653c47dbd5f19c5cd07076e636de9a4c78b207
phuc <tvphuc.11thd@gmail.com>
2026-02-03 22:45:00 +0700

    style: add position for toastr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Toaster />
+        <Toaster position="top-center" richColors />
         {children}
       </body>
     </html>
